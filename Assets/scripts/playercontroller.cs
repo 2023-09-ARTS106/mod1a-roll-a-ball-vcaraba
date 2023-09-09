43685aa85379c1deabc0f510f7498b4f938b72e9
vcaraba <143634264+vcaraba@users.noreply.github.com>
2023-09-08 21:27:02 -0400

    finished

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -2,26 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class playercontroller : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
-    private Rigidbody rb;
     private float movementX;
     private float movementY;
+
+    private Rigidbody rb;
+    private int count;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
-    void OnMove(InputValue movementValue )
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        count = 0;
+
+        SetCountText();
+
+        winTextObject.SetActive(false);
     }
 
     private void FixedUpdate()
@@ -36,8 +40,28 @@ public class playercontroller : MonoBehaviour
         if (other.gameObject.CompareTag("pickup"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
         }
-        
+
     }
+
+    void OnMove(InputValue movementValue )
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >=12)
+            winTextObject.SetActive(true);
+    }
+
+
 
 }
